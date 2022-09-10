0c00d7b5f862fabcf722eb9e75ca075cec4f7997
Marc Hage <tramway.luckier_07@icloud.com>
2022-09-10 10:19:44 +0200

    Tonic

---
+++ index.js
@@ -0,0 +1,68 @@
+import { html } from 'https://gist.githubusercontent.com/marchage/c928c694672f58f30bde44236b8253a1/raw/46830d36935458906dd5eafe9a6650df9ed555c5/'
+
+document.body.appendChild(html`
+<section id="button">
+  <h2>Button</h2>
+  <div id="button-1" class="test-container">
+    <span>Default Button</span>
+    <tonic-button></tonic-button>
+  </div>
+  <div id="button-2" class="test-container">
+    <span>Button has text content</span>
+    <tonic-button>Text Content</tonic-button>
+  </div>
+  <div id="button-2-5" class="test-container">
+    <span>Button has text content</span>
+    <tonic-button value="v"></tonic-button>
+  </div>
+  <div id="button-3" class="test-container">
+    <span>disabled="true"</span>
+    <tonic-button disabled="true">Button</tonic-button>
+  </div>
+  <div id="button-4" class="test-container">
+    <span>disabled="false"</span>
+    <tonic-button disabled="false">Button</tonic-button>
+  </div>
+  <div id="button-5" class="test-container">
+    <span>Has all attributes</span>
+    <tonic-button
+      margin="10px"
+      type="reset"
+      autofocus="true"
+      width="200px"
+      height="50px"
+      tabindex="1"
+      radius="5px"></tonic-button>
+  </div>
+  <div id="button-6" class="test-container">
+    <span>fill="rgb(240, 102, 83)"</span>
+    <tonic-button
+      fill="rgb(240, 102, 83)"
+      text-color="white">Button</tonic-button>
+  </div>
+  <div id="button-7" class="test-container">
+    <span>border-color, border-width, text-color</span>
+    <tonic-button
+      border-color="rgb(240, 102, 83)"
+      border-width="3px">Button</tonic-button>
+  </div>
+  <div id="button-8" class="test-container">
+    <span>async="true"</span>
+    <tonic-button async="true">Button</tonic-button>
+  </div>
+  <div id="button-9" class="test-container">
+    <span>async="false"</span>
+    <tonic-button async="false">Button</tonic-button>
+  </div>
+  <div id="button-10" class="test-container">
+    <span>tabindex="0"</span>
+    <tonic-button tabindex="0">Button</tonic-button>
+  </div>
+  <div id="button-11" class="test-container">
+    <span>href and target</span>
+    <tonic-button target="_blank" href="https://google.com">target="_blank"</tonic-button>
+    <tonic-button target="_self" href="https://google.com">target="_self"</tonic-button>
+    <tonic-button href="https://google.com">No target</tonic-button>
+  </div>
+</section>
+`)
